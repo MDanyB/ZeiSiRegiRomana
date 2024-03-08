478462821e9d1c4266433ebc3c89c95efe5ee056
MDanyB <162067923+MDanyB@users.noreply.github.com>
2024-03-08 07:48:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const Value = document.getElementById("CountLabel")
+
+function increaseNumber() {
+   Value.innerHTML ++
+}
+
+function DecreaseNumber() {
+    Value.innerHTML --
+}
+
+function ResetNumber() {
+    Value.innerHTML = 0
+}
